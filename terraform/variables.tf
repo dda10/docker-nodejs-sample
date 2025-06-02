45690f8a18d421e934e66328e84ad88cdbc4b823
dda10 <ddaduyanh123@gmail.com>
2025-06-02 07:39:22 +0700

    remove noise

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -8,3 +8,14 @@ variable "region" {
   default = "us-central1"
 }
 
+variable "TFC_GCP_PROVIDER_AUTH" {
+  description = "TFC_GCP_PROVIDER_AUTH"
+}
+
+variable "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL" {
+  description = "TFC_GCP_RUN_SERVICE_ACCOUNT_EMAIL"
+}
+
+variable "TFC_GCP_WORKLOAD_PROVIDER_NAME" {
+  description = "TFC_GCP_WORKLOAD_PROVIDER_NAME"
+}
